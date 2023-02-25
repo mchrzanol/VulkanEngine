ecda406ad60dbe34a472c69a60e38ccd5bdbe772
mchrzanol <mchrzanol22@gmail.com>
2023-02-25 19:31:17 +0100

    branch test

--- VulkanEngine/src/Libraries.h
+++ VulkanEngine/src/Libraries.h
@@ -4,14 +4,15 @@
 
 
 // add headers that you want to pre-compile here		////Check how to do precompiled files
-//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #include <windows.h>
 
 //external
-//#define VK_USE_PLATFORM_WIN32_KHR
-//#define GLFW_INCLUDE_VULKAN				// it replace #include <vulkan/vulkan.h>
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN				// it replace #include <vulkan/vulkan.h>
 //#include <GLFW/glfw3.h>
-//#include <glm/glm.hpp>
+//#include <glm/glm.hpp>					// do premake to do this stuff working or somehow do seprate projects for glfw, glm, vulkan and link them by references with engine or
+//#include <glm/gtc/matrix_transform.hpp>	// you must convert those libs to static libs(they are dll now) to link them 
 
 //C++ references
 #include <iostream>
